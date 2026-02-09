6d04f0a8b82423a6d76a4a4c8f10060e4e23867b
parvathygopan <parvathy671999@gmail.com>
2026-02-09 14:21:43 +0530

    corretcd codesss

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Home from "../Pages/Home";
+import Login from "../Pages/Login";
+
 
 const AppRoutes = [
   {
     path: "/",
-    element: <Home />,
+    element: <Login />,
   },
 
 ];
